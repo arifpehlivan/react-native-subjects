f2244f5991bd69f4558fa106715cfde410a69635
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2022-01-11 01:26:06 +0300

    create swipeable tab navigation

---
+++ swipeableTabNavigation/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+function HomePage(){
+  return(
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>Home Page</Text>
+    </View>
+  )
+}
+
+function UserPage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>User Page</Text>
+    </View>
+  );
+}
+
+function OtherPage() {
+  
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Other Page </Text>
+    </View>
+  );
+}
+
+
+
+const App=()=>{
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+       <Tab.Screen name="Home" options={ {headerTitleAlign: 'center'}} component={HomePage} />
+      <Tab.Screen name="User" options={ {headerTitleAlign: 'center'}} component={UserPage} />
+      <Tab.Screen name="Other" options={ {headerTitleAlign: 'center'}} component={OtherPage} /> 
+    </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
