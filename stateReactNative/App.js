333395e76d0a9465b9300152905eeb7c1703945d
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2022-01-16 02:24:45 +0300

    add color

--- stateReactNative/App.js
+++ stateReactNative/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Text, View, Button, SafeAreaView } from "react-native";
+import { Text, View, Button, SafeAreaView} from "react-native";
 
 function App(){
   const [counter, setCounter] = useState(0);
@@ -13,10 +13,12 @@ function App(){
   return(
     <SafeAreaView>
       <Text>Counter: {counter}</Text>
-      <Button title="increase counter" onPress={increaseCounter}></Button>
-      <Button title="decrease counter" onPress={decreaseCounter}></Button>
+      <Button  title="increase counter" onPress={increaseCounter} color="black"></Button>
+      <Button  title="decrease counter" onPress={decreaseCounter} color="blue"></Button>
     </SafeAreaView>
   )
 }
+
+
 
 export default App
